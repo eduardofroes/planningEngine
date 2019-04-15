83eaea713d6982ea9d7889b1bbe78ff84fb573fd
Eduardo Froes <eduardomfroes@gmail.com>
2019-04-14 23:05:25 -0300

    Fixing token regex.

--- src/lexical/token.go
+++ src/lexical/token.go
@@ -7,46 +7,51 @@ import (
 type Token struct {
 	Name       string
 	Expression string
+	ValueFound string
 }
 
-var If = Token{Name: "if", Expression: fmt.Sprintf("\\s+(if)\\s*%s", OpenParenthese.Expression)}
-var Else = Token{Name: "else", Expression: "\\s*(else)\\s*\\("}
+var Function = Token{Name: "Func", Expression: `\s*(func)\s+`}
+var If = Token{Name: "If", Expression: fmt.Sprintf(`\s+(if)\s*%s`, OpenParenthese.Expression)}
+var Else = Token{Name: "Else", Expression: `\s*(else)\s*\(`}
 var OpenParenthese = Token{Name: "OpenParenthese", Expression: `\(`}
 var CloseParenthese = Token{Name: "CloseParenthese", Expression: `\)`}
-var Equality = Token{Name: "Equality", Expression: "=="}
-var Inequality = Token{Name: "Inequality", Expression: "!="}
-var Less = Token{Name: "Less", Expression: "<"}
-var LessEqual = Token{Name: "LessEqual", Expression: "<="}
-var Great = Token{Name: "Great", Expression: ">"}
-var GreatEqual = Token{Name: "GreatEqual", Expression: ">="}
-var Not = Token{Name: "Not", Expression: "!"}
+var Equality = Token{Name: "Equality", Expression: `\=\=`}
+var Inequality = Token{Name: "Inequality", Expression: `\!\=`}
+var Less = Token{Name: "Less", Expression: `\<`}
+var LessEqual = Token{Name: "LessEqual", Expression: `\<\=`}
+var Great = Token{Name: "Great", Expression: `\>`}
+var GreatEqual = Token{Name: "GreatEqual", Expression: `\>\=`}
+var Not = Token{Name: "Not", Expression: `\!`}
 var OpenBrace = Token{Name: "OpenBrace", Expression: `\{`}
 var CloseBrace = Token{Name: "CloseBrace", Expression: `\}`}
-var Dot = Token{Name: "Dot", Expression: "."}
-var And = Token{Name: "And", Expression: "&&"}
-var Or = Token{Name: "Or", Expression: "||"}
-var Identifier = Token{Name: "Identifier", Expression: ""}
-var IdentifierName = Token{Name: "IdentifierName", Expression: "[a-zA-Z]\\w*"}
-var Attribute = Token{Name: "Attribute", Expression: "Attribute"}
-var AttributeName = Token{Name: "AttributeName", Expression: "[a-zA-Z]\\w*"}
-var Value = Token{Name: "Value", Expression: "Value"}
-var IntValue = Token{Name: "IntValue", Expression: "[0-9]+"}
-var FloatValue = Token{Name: "FloatValue", Expression: "[0-9]+.[0-9+]"}
-var StringValue = Token{Name: "StringValue", Expression: "\"\\w*\""}
-var Plus = Token{Name: "Plus", Expression: "\\+"}
-var Minus = Token{Name: "Minus", Expression: "\\-"}
-var Multiply = Token{Name: "Multiply", Expression: "\\*"}
-var Divide = Token{Name: "Divide", Expression: "\\/"}
-var Rest = Token{Name: "Rest", Expression: "\\%"}
-var Save = Token{Name: "Save", Expression: "save"}
-var Delete = Token{Name: "Delete", Expression: "delete"}
-var Get = Token{Name: "Get", Expression: "get"}
-var Return = Token{Name: "Return", Expression: "return"}
-var Input = Token{Name: "Input", Expression: "input.[a-zA-Z]+"}
-var Memory = Token{Name: "Memory", Expression: "memory.[a-zA-Z]+"}
+var Dot = Token{Name: "Dot", Expression: `\.`}
+var And = Token{Name: "And", Expression: `\s+\&\&\s+`}
+var Or = Token{Name: "Or", Expression: `\s+\|\|\s+`}
+var Identifier = Token{Name: "Identifier", Expression: `Identifier\s+`}
+
+//var IdentifierName = Token{Name: "IdentifierName", Expression: "[a-zA-Z]\\w*"}
+var Attribute = Token{Name: "Attribute", Expression: `Attribute\s+`}
+
+//var AttributeName = Token{Name: "AttributeName", Expression: "[a-zA-Z]\\w*"}
+var Value = Token{Name: "Value", Expression: `Value\s+`}
+var IntValue = Token{Name: "IntValue", Expression: `\s+[0-9]+`}
+var FloatValue = Token{Name: "FloatValue", Expression: `\s+[0-9]+.[0-9+]`}
+var StringValue = Token{Name: "StringValue", Expression: `\"\w*\"`}
+var Plus = Token{Name: "Plus", Expression: `\+`}
+var Minus = Token{Name: "Minus", Expression: `\-`}
+var Multiply = Token{Name: "Multiply", Expression: `\*`}
+var Divide = Token{Name: "Divide", Expression: `\/`}
+var Rest = Token{Name: "Rest", Expression: `\%`}
+var Save = Token{Name: "Save", Expression: `save\s+`}
+var Delete = Token{Name: "Delete", Expression: `delete\s+`}
+var Get = Token{Name: "Get", Expression: `get\s+`}
+var Return = Token{Name: "Return", Expression: `return\s+`}
+var Input = Token{Name: "Input", Expression: `input\.[a-zA-Z]+\s+`}
+var Memory = Token{Name: "Memory", Expression: `memory\.[a-zA-Z]+\s+`}
 
 func GetAllTokens() []Token {
 	tokens := []Token{
+		Function,
 		If,
 		Else,
 		OpenParenthese,
@@ -63,9 +68,9 @@ func GetAllTokens() []Token {
 		And,
 		Or,
 		Identifier,
-		IdentifierName,
+		//	IdentifierName,
 		Attribute,
-		AttributeName,
+		//	AttributeName,
 		Value,
 		IntValue,
 		FloatValue,
--- src/lexical/tokenizer.go
+++ src/lexical/tokenizer.go
@@ -3,56 +3,34 @@ package lexical
 import (
 	"fmt"
 	"regexp"
-	"strings"
-
-	"github.com/sirupsen/logrus"
 )
 
 // LexicalAnalyzer Structure used to analyze rules statements.
 type LexicalAnalyzer struct {
 	InputRule string
+	TokensFound []Token
 }
 
-func (l LexicalAnalyzer) Analyze(ruleString string) []Token {
-
-	wordsRule := strings.Split(ruleString, " ")
-
-	for _, frase := range wordsRule {
-		word := ""
-		for i, c := range frase {
-			character := string(c)
-			tokens := GetAllTokens()
-			logrus.Info(tokens)
-
-			word = word + character
-			if len(ruleString) >= i+1 {
-				for _, token := range GetAllTokens() {
-					sent := regexp.MustCompile(fmt.Sprintf(`%s`, token.Expression))
-					indexes := sent.FindAllIndex([]byte(ruleString), 2)
-					//matched, err := regexp.Match(fmt.Sprintf(`\A(#{%s})/`, token), []byte(ruleString))
-					fmt.Println(fmt.Sprintf("%s - %v", token, indexes))
-				}
+func (l LexicalAnalyzer) Tokenize(ruleString string) ([]Token, error) {
+	tokensFound := []Token{}
+	word := ""
+	for _, c := range ruleString {
+		character := string(c)
+		tokens := GetAllTokens()
+		word = word + character
+		for _, token := range tokens {
+			matched, err := regexp.Match(fmt.Sprintf(`%s`, token.Expression), []byte(word))
+			if err != nil {
+				return nil, err
+			}
+			if matched {
+				tokensFound = append(tokensFound, Token{Name:token.Name, Expression:token.Expression, ValueFound:word})
+				word = ""
 			}
 		}
 	}
 
-	return []Token{}
-}
+	fmt.Println(fmt.Sprintf("Test: %v", tokensFound))
 
-func (l LexicalAnalyzer) Tokenize(ruleString string) {
-
-	//tokens := GetAllTokens()
-
-	word := ""
-	for _, c := range ruleString {
-		word = word + string(c)
-
-	}
-
-	// for _, token := range GetAllTokens() {
-	// 	sent := regexp.MustCompile(fmt.Sprintf(`%s`, token.Expression))
-	// 	indexes := sent.FindAllIndex([]byte(ruleString), 2)
-	// 	//matched, err := regexp.Match(fmt.Sprintf(`\A(#{%s})/`, token), []byte(ruleString))
-	// 	fmt.Println(fmt.Sprintf("%s - %v", token, indexes))
-	// }
+	return tokensFound, nil
 }
