fc7d1eca26e48e223e2e63adec1940d92fdd847e
Eduardo de Moraes Froes <eduardo.froes@ifood.com.br>
2019-05-06 20:11:48 -0300

    Syntactic process improvements.

--- src/syntactical/syntactic.go
+++ src/syntactical/syntactic.go
@@ -1,7 +1,9 @@
 package syntactical
 
 import (
-	"planningEngine/src/lexical"
+	"planningEngine/lexical"
+
+	"github.com/sirupsen/logrus"
 )
 
 type SyntacticalAnalyzer struct {
@@ -11,101 +13,112 @@ type SyntacticalAnalyzer struct {
 }
 
 type Sequence struct {
-	Name        string
 	TokensCheck [][]lexical.Token
 }
 
+//SyntacticValidation is responsible for validate whether the rules are syntactically correct.
+func (s *SyntacticalAnalyzer) SyntacticValidation(tokens []lexical.Token) bool {
+
+	sequenceMap := GetAllSequence()
+	for _, token := range tokens {
+		tokensMatrix := sequenceMap[token.Name]
+		logrus.Info("%v", tokensMatrix)
+	}
+
+	return true
+}
+
 // GetAllSequence is responsible to gets all sequence which is allowed in syntactical analyzer.
-func GetAllSequence() []Sequence {
-	return []Sequence{
-		{
-			Name: lexical.If.Name,
-			TokensCheck: [][]lexical.Token{
-				{
-					lexical.If,
-					lexical.OpenParentheses,
-					lexical.SubSequence,
-					lexical.CloseParentheses,
-					lexical.OpenBrace,
-					lexical.SubSequence,
-					lexical.CloseBrace,
-				},
-				{
-					lexical.If,
-					lexical.SubSequence,
-					lexical.OpenBrace,
-					lexical.SubSequence,
-					lexical.CloseBrace,
-				},
+func GetAllSequence() map[string]Sequence {
+
+	sequenceMap := make(map[string]Sequence)
+
+	sequenceMap[lexical.If.Name] = Sequence{
+		TokensCheck: [][]lexical.Token{
+			{
+				lexical.If,
+				lexical.OpenParentheses,
+				lexical.SubSequence,
+				lexical.CloseParentheses,
+				lexical.OpenBrace,
+				lexical.SubSequence,
+				lexical.CloseBrace,
 			},
-		},
-		{
-			Name: lexical.Propose.Name,
-			TokensCheck: [][]lexical.Token{
-				{
-					lexical.Propose,
-					lexical.Entity,
-					lexical.OpenParentheses,
-					lexical.SubSequence,
-					lexical.CloseParentheses,
-					lexical.OpenBrace,
-					lexical.SubSequence,
-					lexical.CloseBrace,
-				},
-			},
-		},
-		{
-			Name: lexical.Apply.Name,
-			TokensCheck: [][]lexical.Token{
-				{
-					lexical.Apply,
-					lexical.Entity,
-					lexical.OpenParentheses,
-					lexical.SubSequence,
-					lexical.CloseParentheses,
-					lexical.OpenBrace,
-					lexical.SubSequence,
-					lexical.CloseBrace,
-				},
-			},
-		},
-		{
-			Name: lexical.Identifier.Name,
-			TokensCheck: [][]lexical.Token{
-				{
-					lexical.Identifier,
-					lexical.Entity,
-				},
-				{
-					lexical.Identifier,
-					lexical.Entity,
-					lexical.Comma,
-				},
-			},
-		},
-		{
-			Name: "Assignment",
-			TokensCheck: [][]lexical.Token{
-				{
-					lexical.Entity,
-					lexical.Equal,
-					lexical.FloatValue,
-				},
-				{
-					lexical.Entity,
-					lexical.Equal,
-					lexical.IntValue,
-				},
-				{
-					lexical.Entity,
-					lexical.Equal,
-					lexical.DoubleQuotes,
-					lexical.Entity,
-					lexical.DoubleQuotes,
-				},
+			{
+				lexical.If,
+				lexical.SubSequence,
+				lexical.OpenBrace,
+				lexical.SubSequence,
+				lexical.CloseBrace,
 			},
 		},
 	}
+
+	sequenceMap[lexical.Propose.Name] = Sequence{
+		TokensCheck: [][]lexical.Token{
+			{
+				lexical.Propose,
+				lexical.Entity,
+				lexical.OpenParentheses,
+				lexical.SubSequence,
+				lexical.CloseParentheses,
+				lexical.OpenBrace,
+				lexical.SubSequence,
+				lexical.CloseBrace,
+			},
+		},
+	}
+
+	sequenceMap[lexical.Apply.Name] = Sequence{
+		TokensCheck: [][]lexical.Token{
+			{
+				lexical.Apply,
+				lexical.Entity,
+				lexical.OpenParentheses,
+				lexical.SubSequence,
+				lexical.CloseParentheses,
+				lexical.OpenBrace,
+				lexical.SubSequence,
+				lexical.CloseBrace,
+			},
+		},
+	}
+
+	sequenceMap[lexical.Identifier.Name] = Sequence{
+		TokensCheck: [][]lexical.Token{
+			{
+				lexical.Identifier,
+				lexical.Entity,
+			},
+			{
+				lexical.Identifier,
+				lexical.Entity,
+				lexical.Comma,
+			},
+		},
+	}
+
+	sequenceMap["Assingment"] = Sequence{
+		TokensCheck: [][]lexical.Token{
+			{
+				lexical.Entity,
+				lexical.Equal,
+				lexical.FloatValue,
+			},
+			{
+				lexical.Entity,
+				lexical.Equal,
+				lexical.IntValue,
+			},
+			{
+				lexical.Entity,
+				lexical.Equal,
+				lexical.StringValue,
+			},
+		},
+	}
+
+	return sequenceMap
 }
 
 func (s SyntacticalAnalyzer) SyntacticalAnalyzer() (bool, error) {
